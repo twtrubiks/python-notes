ffe6f997143b8deb0bdc77a33ed7b8c18c342ec6
twtrubiks <twtrubiks@gmail.com>
2018-07-24 23:30:17 +0800

    update namedtuple_tutorial.py

--- namedtuple_tutorial.py
+++ namedtuple_tutorial.py
@@ -11,10 +11,22 @@ if __name__ == "__main__":
     print('=== use namedtuple ===')
     NetworkAddress = namedtuple('NetworkAddress', ['hostname', 'port'])  # write 1
     # NetworkAddress = namedtuple('NetworkAddress', 'hostname port') # write 2
+    print('NetworkAddress._fields', NetworkAddress._fields)  # _fields is a tuple with the field names of the class
+
     a = NetworkAddress('localhost', 3306)
     print('a.hostname:', a.hostname)
     print('a.port:', a.port)
     # a.port = 80  # error , tuple read only
+
+    network_address_data = ('hello', 1234)
+    na_1 = NetworkAddress._make(network_address_data)  # write 1
+    na_2 = NetworkAddress(*network_address_data)  # write 2
+    print('na_1', na_1)
+    print('na_2', na_2)
+
+    # _asdict() return a collections.OrderedDict
+    # That can be used to produce a nice display of data
+    print('na_1._asdict()', na_1._asdict())
 
     print('=== use class ===')
     b = NetworkAddressClass('localhost', 3306)
