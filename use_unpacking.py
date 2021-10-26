28b4a7ba21670e06ec44fb84143dc4f6d88c5d16
twtrubiks <twtrubiks@gmail.com>
2021-10-26 20:51:31 +0800

    unpacking dict tutorial

--- use_unpacking.py
+++ use_unpacking.py
@@ -1,4 +1,4 @@
-if __name__ == "__main__":
+def example1():
     # Not using explicit unpacking
     # elems = [4, 7, 18]
     #
@@ -14,3 +14,41 @@ if __name__ == "__main__":
     tuple_data = (0, 1, 2)
     tuple0, tuple1, tuple2 = tuple_data
     print(tuple0, tuple1, tuple2)
+
+
+# Unpacking Operator
+# *   -> Iterable
+# **  -> Dictionary
+
+def example2():
+    # Packing With the * Operator
+
+    *c, = 1, 2
+    print('c', c)
+
+    a, *b = 1, 2, 3
+    print('a', a)
+    print('b', b)
+
+def example3():
+    # *   -> Iterable
+    *r, = range(5)
+    # *r = range(5) # <- SyntaxError: starred assignment target must be in a list or tuple
+    print(r)
+
+def example4():
+    # **  -> Dictionary
+    # Unpacking Dictionaries With the ** Operator
+
+    dict_1 = {"one": 1, "two": 2, "three": 3}
+    dict_2 = {"a": "A", "b": "B", "c": "C"}
+    merge_1 = {**dict_1, **dict_2}
+    print(merge_1)
+    merge_2 = {**{"a": 0, "b": 2, "c": 3}, **{"A": 3, "B": 2, "C": 1}}
+    print(merge_2)
+
+if __name__ == "__main__":
+    example1()
+    # example2()
+    # example3()
+    # example4()
