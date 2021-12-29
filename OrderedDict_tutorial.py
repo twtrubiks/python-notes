356807743310d1b292b1bdc134b87e0d4dedeab5
twtrubiks <twtrubiks@gmail.com>
2021-12-29 16:27:53 +0800

    upgrade OrderedDict

--- OrderedDict_tutorial.py
+++ OrderedDict_tutorial.py
@@ -1,19 +1,10 @@
+# Python 3.6 introduced a new implementation of dict.
+# dict now keeps its items ordered as well.
+
 # An OrderedDict is a dictionary subclass that remembers the order in which its contents are added.
 from collections import OrderedDict
 
 if __name__ == "__main__":
-    print('Regular dictionary:')
-    regular_dic = dict()
-    regular_dic['a'] = 'A'
-    regular_dic['b'] = 'B'
-    regular_dic['c'] = 'C'
-    regular_dic['d'] = 'D'
-    regular_dic['e'] = 'E'
-
-    for k, v in regular_dic.items():
-        print k, v
-
-    print('OrderedDict:')
     order_dic = OrderedDict()
     order_dic['a'] = 'A'
     order_dic['b'] = 'B'
@@ -22,4 +13,27 @@ if __name__ == "__main__":
     order_dic['e'] = 'E'
 
     for k, v in order_dic.items():
-        print k, v
+        print(k, v)
+
+    # https://docs.python.org/3/library/collections.html#collections.OrderedDict.popitem
+    # last=True -> LIFO
+    # last=False -> FIFO
+
+    print(
+        order_dic.popitem(last=True) # -> ('e', 'E')
+    )
+
+    print(
+        order_dic.popitem(last=False) # -> ('a', 'A')
+    )
+
+
+    # https://docs.python.org/3/library/collections.html#collections.OrderedDict.move_to_end
+    # last=True   -> The item is moved to the right end
+    # last=False  -> The item is moved to the beginning
+
+    print('origin:', order_dic) # OrderedDict([('b', 'B'), ('c', 'C'), ('d', 'D')])
+    order_dic.move_to_end('b', last=True)
+    print(order_dic) # OrderedDict([('c', 'C'), ('d', 'D'), ('b', 'B')])
+    order_dic.move_to_end('b', last=False)
+    print(order_dic) # OrderedDict([('b', 'B'), ('c', 'C'), ('d', 'D')])
